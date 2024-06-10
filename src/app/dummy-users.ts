2b5ca83111a7ef0a4b2804aa4b96ab36bd79b06e
sbatthineni <sbatthineni@firehydrantmobile.com>
2024-06-10 14:23:35 -0400

    task tracker app_angular_2.0

--- src/app/dummy-users.ts
+++ src/app/dummy-users.ts
@@ -1,9 +1,9 @@
 export const DUMMY_USERS = [
   
   { id: 'u1', name: 'Michelle Obama', avatar: 'user-1.jpg' },
-  { id: 'u2', name: 'Tim Brooke', avatar: 'user-2.jpg' },
-  { id: 'u3', name: 'Marcus Johnson', avatar: 'user-3.jpg' },
-  { id: 'u4', name: 'David Miller', avatar: 'user-4.jpg' },
+  { id: 'u2', name: 'Shilpa Batthineni', avatar: 'user-2.jpg' },
+  { id: 'u3', name: 'DJ Bravo', avatar: 'user-3.jpg' },
+  { id: 'u4', name: 'Steve Smith', avatar: 'user-4.jpg' },
   { id: 'u5', name: 'Priya Patel', avatar: 'user-5.jpg' },
   { id: 'u6', name: 'Arjun Singh', avatar: 'user-6.jpg' },
 ];
